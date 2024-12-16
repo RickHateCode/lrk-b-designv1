023561c3c36cea3069c59e8bd42e03e0a7b80054
rickHateCode <hanonyme99@gmail.com>
2024-12-16 11:33:40 +0100

    my commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,10 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "vector" : "url('src/assets/vector.jpg')",
-        "flower" : "url('/flower.jpg')",
-        "construction": "url('/src/assets/construction.jpg')",
-        "arc": "url('/src/assets/arc-divider.svg')",
-        "bgAbout": "url('/lrk-b-designv1/src/assets/Bgabout.jpg')",
-        "bgService": "url('/lrk-b-designv1/src/assets/BgService.jpg')",
-        "bgProverb": "url('/lrk-b-designv1/src/assets/background.jpg')",
-        "bgAfrican": "url('/lrk-b-designv1/src/assets/africanMan.jpg')",
+        "bgAbout": "url('/lrk-b-designv1/Bgabout.jpg')",
+        "bgService": "url('/lrk-b-designv1/BgService.jpg')",
+        "bgProverb": "url('/lrk-b-designv1/background.jpg')",
+        "bgAfrican": "url('/lrk-b-designv1/africanMan.jpg')",
       }
     },
   },
